8a4751306d77104f810249ad0cd7f8901bbd5d12
muchizmo <muchizmo@gmail.com>
2012-10-06 22:16:52 +0600

    initial rails 3 app skeleton commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyReads::Application.config.secret_token = '4bdf22a0ce127eb5383d0ff85a6caf81a0cf31e584874e4dea319c8c05fcc65eeac08220f2edd010f87e5f65f8812cf9afc11ad670c61d092739ed7155a48417'
